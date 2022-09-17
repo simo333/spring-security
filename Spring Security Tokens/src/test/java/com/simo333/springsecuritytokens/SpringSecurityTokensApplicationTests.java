d766fd71c66d6151868df17f997e7d8f43a0decd
simo333 <szymon.333@wp.pl>
2022-09-17 10:20:05 +0200

    Initial Commit

---
+++ Spring Security Tokens/src/test/java/com/simo333/springsecuritytokens/SpringSecurityTokensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simo333.springsecuritytokens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTokensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
